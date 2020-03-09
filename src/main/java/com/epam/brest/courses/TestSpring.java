6c632437a5ac310baba0b2a3c2638ef9d3b1bc1f
Aliaksei Vazdusevich <avozdusevich@gmail.com>
2020-03-09 23:09:29 +0300

    java.config implemented xml.config deleted

--- src/main/java/com/epam/brest/courses/TestSpring.java
+++ src/main/java/com/epam/brest/courses/TestSpring.java
@@ -1,17 +1,12 @@
 package com.epam.brest.courses;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
 
-        // Доступ к классу ClassPathXmlApplicationContext мы получили благодаря подключению зависимости springframework
-        // Он обращается к файлу applicationContext.xml, считывает его и помечает все классы,
-        // которые будут помещены в context в качестве бинов
-
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
 //        Computer computer = context.getBean("computer", Computer.class);
 //        System.out.println(computer);
